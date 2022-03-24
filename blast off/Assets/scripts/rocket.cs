3b1a7c573cfc69c62e7e36690034c4a72afc4f60
Fajer Alammar <alammarfajer@gmail.com>
2022-03-24 15:47:43 +0300

    done

--- blast off/Assets/scripts/rocket.cs
+++ blast off/Assets/scripts/rocket.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class rocket : MonoBehaviour
 {
@@ -8,7 +9,11 @@ public class rocket : MonoBehaviour
     Rigidbody rocketRB;
     AudioSource rocketAudioSource;
 
+    //SeialixeFields
     [SerializeField] AudioClip mainEngine;
+    [SerializeField] float thrustSpeed = 5.0f;
+    [SerializeField] float rotationSpeed = 2.5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,7 +34,7 @@ public class rocket : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             print("Thrusting");
-            rocketRB.AddRelativeForce(Vector3.up * 60);
+            rocketRB.AddRelativeForce(Vector3.up * thrustSpeed);
             if (!rocketAudioSource.isPlaying)
             {
                 rocketAudioSource.PlayOneShot(mainEngine);
@@ -39,15 +44,35 @@ public class rocket : MonoBehaviour
             rocketAudioSource.Stop();
         }
     }
+
+     void OnCollisionEnter(Collision collision)
+    {
+        print(collision.gameObject.tag);
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("No problem");
+                break;
+            case "Finish":
+                print("You win!");
+                SceneManager.LoadScene(0);
+                break;
+                default:
+                print("You lose!");
+                SceneManager.LoadScene(1);
+                break;
+        }
+    }
+
     void Rotate()
     {
         if(Input.GetKey(KeyCode.A))
         {
-            print("Rotating left");
-            transform.Rotate(Vector3.forward);
+            //print("Rotating left");
+            transform.Rotate(Vector3.forward *rotationSpeed);
         }else if (Input.GetKey(KeyCode.D)) {
-            print("Rotating right");
-            transform.Rotate(-Vector3.forward);
+            //print("Rotating right");
+            transform.Rotate(-Vector3.forward *rotationSpeed);
         }
     }
 }
